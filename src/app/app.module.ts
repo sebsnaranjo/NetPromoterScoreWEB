ad613e6ff3c0caa68e2df070214c4e927d109e5f
Sebastian Naranjo <sebas2013n@gmail.com>
2024-05-21 11:02:07 -0500

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { CalificationComponent } from './components/calification/calification.component';
 import { CalificationListComponent } from './components/calification-list/calification-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     CalificationComponent,
-    CalificationListComponent
+    CalificationListComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
